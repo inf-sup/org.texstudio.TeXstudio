7aae2457192c1167e3af5ff5cc44709bf36f5ae8
inf-sup <174466778+inf-sup@users.noreply.github.com>
2024-08-12 11:45:17 +0800

    feat: remove utilities/dictionaries
    删除了大部分语言的自动检查

--- prepare/workdir/build.sh
+++ prepare/workdir/build.sh
@@ -1,6 +1,6 @@
 # install packages
 install_pkg=$(realpath "./install_pkg.sh")
-include_pkg=''
+include_pkg='liblcms2-2'
 exclude_pkg=''
 bash $install_pkg -i -d $(realpath 'linglong/sources') -p $PREFIX -I \"$include_pkg\" -E \"$exclude_pkg\"
 export LD_LIBRARY_PATH=$PREFIX/lib/$TRIPLET:$LD_LIBRARY_PATH
@@ -10,13 +10,15 @@ cd /project/linglong/sources/poppler.git
 cmake -Bbuild \
       -DCMAKE_INSTALL_PREFIX=/project/linglong/sources/poppler.git/build_ins \
       -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib/$TRIPLET \
-      -DCMAKE_BUILD_TYPE=release
+      -DCMAKE_BUILD_TYPE=release \
+      -DENABLE_QT6=OFF
 cd build
 make -j$(nproc)
 make install
 
 # build TeXstudio
 cd /project/linglong/sources/texstudio.git
+sed -i '474c \		REGEX .*en_US.*|.*fr_FR.*|.*ru_RU.*' CMakeLists.txt
 mkdir build
 cd build
 . ../.github/scripts/get-version.sh
@@ -26,3 +28,6 @@ cmake --build . --target install -- -j 2
 
 # uninstall dev packages
 bash $install_pkg -u -r '\-dev' -D
+
+rm -r $PREFIX/lib/$TRIPLET/pkgconfig $PREFIX/share/pkgconfig $PREFIX/share/man $PREFIX/share/doc
+strip -s $PREFIX/bin/* $PREFIX/lib/$TRIPLET/*
